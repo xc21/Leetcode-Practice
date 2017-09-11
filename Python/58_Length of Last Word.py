459df535a83d38e7b533dde04cc576cb9e376eb3
xc21 <xc21@rice.edu>
2017-09-10 21:55:56 -0500

    Update 58_Length of Last Word.py

--- Python/58_Length of Last Word.py
+++ Python/58_Length of Last Word.py
@@ -5,8 +5,19 @@ Created on Sun Sep 10 21:36:49 2017
 @author: Xun Cao
 """
 
+#36 ms
 class Solution:
     # @param s, a string
     # @return an integer
     def lengthOfLastWord(self, s):
         return len(s.split()[len(s.split())-1]) if s.split() != [] else 0
+#42 ms
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def lengthOfLastWord(self, s):
+        length=0
+        if s.split()!=[]:
+                               #at last digit
+            length=len(s.split()[len(s.split())-1])
+        return length
